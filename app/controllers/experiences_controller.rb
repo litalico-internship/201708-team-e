0cdd888774766d4d124557dd819abf44d104ee8b
mba2014early <mba2014early@example.com>
2017-08-20 04:48:27 +0900

    レビューをUser#showで投稿できるようにした

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -5,7 +5,6 @@ class ExperiencesController < ApplicationController
 
   def show
     @experience = Experience.find(params[:id])
-    @reviews = Review.all
   end
 
   def new
@@ -24,23 +23,7 @@ class ExperiencesController < ApplicationController
     end
   end
 
-  def update
-    @experience = Experience.find(params[:id])
-    @review = Review.new(text: review_params[:reviews][:text], rate: review_params[:reviews][:rate], user_id: current_user.id)
-
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to @experience, notice: 'Review was successfully created.' }
-      else
-        format.html { render :show }
-      end
-    end
-  end
-
   private
-    def review_params
-      params.require(:experience).permit(reviews: [:text, :rate])
-    end
 
     def experience_params
       params.require(:experience).permit(:title, :date, :place, :image, :content)
