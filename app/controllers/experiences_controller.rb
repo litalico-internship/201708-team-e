a5c93608dfb9debc9d47d2287216b321b5bdc2eb
valai <goriakku@gmail.com>
2017-08-19 21:54:43 +0900

    new画面の作成途中

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,5 +1,13 @@
 class ExperiencesController < ApplicationController
+  def create
+    experience = params.require(:experience).permit(:title, :host, :date, :place, :content)
+    experience.save
+  end
+
+  def new
+  end
 
   def index
+
   end
 end
