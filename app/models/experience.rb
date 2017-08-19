ea45cfdf73eeb5d5ebdb8cd32b3c6a7c055107ec
mba2014early <mba2014early@example.com>
2017-08-20 02:07:06 +0900

    User/Experienceのアソシエーションを記述

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,2 +1,4 @@
 class Experience < ApplicationRecord
+  has_many :users, through: :user_groups
+  has_many :user_groups
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
   has_many :reviews, through: :user_reviews
   has_many :user_reviews
 
+  has_many :experiences, through: :user_groups
+  has_many :user_groups
+
   validates :name, presence: true
 end
