84ebe469ad6efb325d540e427a531e01896afbe0
mba2014early <mba2014early@example.com>
2017-08-20 01:26:21 +0900

    Userモデルにdetailカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819161102) do
+ActiveRecord::Schema.define(version: 20170819162359) do
 
   create_table "experiences", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170819161102) do
     t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "detail"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
