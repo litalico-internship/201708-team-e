7da021e97050e8bd5dc834437c3c78384860a81b
valai <goriakku@gmail.com>
2017-08-19 23:36:39 +0900

    experienceの作成を実装

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,7 +1,15 @@
 class ExperiencesController < ApplicationController
   def create
-    experience = params.require(:experience).permit(:title, :host, :date, :place, :content)
-    experience.save
+    # experience = params.require(:experience).permit(:title, :host, :date, :place, :content)
+    @experience = Experience.new(experience_params)
+
+    respond_to do |format|
+      if @experience.save
+        format.html { redirect_to @experience, notice: 'Experience was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def new
@@ -9,5 +17,11 @@ class ExperiencesController < ApplicationController
 
   def index
 
+  end
+
+  private
+
+  def experience_params
+    params.require(:experience).permit(:title, :date, :place, :image, :content)
   end
 end
