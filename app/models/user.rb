82316fc9989cbe09f9d6bff49507dc48a84ccd76
前山　大次郎 <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2017-08-20 02:10:41 +0900

    サインアップをする際に画像をアップロードできるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   has_many :user_reviews
 
   validates :name, presence: true
+
+  has_attached_file :avatar,
+                      styles:  { medium: "300x300#", thumb: "100x100#" }
+  validates_attachment_content_type :avatar,
+                                      content_type: ["image/jpg","image/jpeg","image/png"]
 end
